c7a5f4adeacabdf224336ba7d42c803b993c2cdd
ChenZhongPu <18789482356@163.com>
2013-10-05 13:39:25 +0800

    R.java

---
+++ R.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class R{
+	static <T> void fromArrayToColl(T[] arr, Collection<T> col)
+	{
+		for ( T o: arr)
+		{
+			col.add(o);
+		}
+	}
+	public void test(List<?> c)
+	{
+		for(Object obj : c)
+			System.out.println(c);
+	}
+	public static void main(String[] args)
+	{
+		List<String> strList = new ArrayList<>();
+		new R().test(strList);
+
+		List<?> oList = strList;
+	//	oList.add(2,"hi");
+	}
+}
